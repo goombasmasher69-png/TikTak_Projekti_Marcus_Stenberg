bad800b2737dc3e1cca3e9f6494767cd3f015f6e
Oppilas <Oppilas@DESKTOP-JJB91AH>
2026-03-12 09:05:42 +0200

    Add project files.

---
+++ TikTak_Projekti_Marcus_Stenberg/Logic.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace TikTak_Projekti_Marcus_Stenberg
+{
+    public class GameLogic
+    {
+        // Board property
+        public char[] Board { get; set; }
+
+        // Current player property
+        public char CurrentPlayer { get; set; }
+
+        // Constructor
+        public GameLogic()
+        {
+            Board = new char[9];
+
+            // Fill board with empty spaces
+            for (int i = 0; i < 9; i++)
+            {
+                Board[i] = ' ';
+            }
+
+            CurrentPlayer = 'X';
+        }
+
+        // Method to make a move
+        public void MakeMove(int position)
+        {
+            if (position >= 1 && position <= 9)
+            {
+                if (Board[position - 1] == ' ')
+                {
+                    Board[position - 1] = CurrentPlayer;
+
+                    if (CurrentPlayer == 'X')
+                    {
+                        CurrentPlayer = 'O';
+                    }
+                    else
+                    {
+                        CurrentPlayer = 'X';
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Position already taken.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid position.");
+            }
+        }
+    }
+
+    public class BoardPrinter
+    {
+        // Method to print the board
+        public void PrintBoard(char[] board)
+        {
+            Console.WriteLine();
+
+            Console.WriteLine(" " + board[0] + " | " + board[1] + " | " + board[2]);
+            Console.WriteLine("---+---+---");
+            Console.WriteLine(" " + board[3] + " | " + board[4] + " | " + board[5]);
+            Console.WriteLine("---+---+---");
+            Console.WriteLine(" " + board[6] + " | " + board[7] + " | " + board[8]);
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ TikTak_Projekti_Marcus_Stenberg/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TikTak_Projekti_Marcus_Stenberg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask user name
+            Console.Write("Enter your name: ");
+            string playerName = Console.ReadLine();
+
+            // Save date and time
+            DateTime startTime = DateTime.Now;
+
+            // Print welcome message
+            Console.WriteLine("Welcome to Tic Tac Toe " + playerName);
+            Console.WriteLine("Game started at: " + startTime);
+
+            // Create objects
+            GameLogic game = new GameLogic();
+            BoardPrinter printer = new BoardPrinter();
+
+            // Game loop
+            while (true)
+            {
+                // Print board
+                printer.PrintBoard(game.Board);
+
+                // Ask move
+                Console.Write("Enter position (1-9) or 0 to quit: ");
+                int move = int.Parse(Console.ReadLine());
+
+                // Exit program
+                if (move == 0)
+                {
+                    break;
+                }
+
+                // Make move
+                game.MakeMove(move);
+            }
+
+            // Exit message
+            Console.WriteLine("Program closing. Thank you for playing.");
+        }
+    }
+}
